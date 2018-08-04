4dd88e0f794824f53c9f4d26876e267d37dc6d4c
aleksanderbr <40921880+aleksanderbr@users.noreply.github.com>
2018-08-04 18:29:09 +0200

    #2

--- script.js
+++ script.js
@@ -22,6 +22,18 @@ var todoList = {
     todos: ["item 1", "item 2", "item 3"],
     displayTodos: function(){
         console.log("My Todos: ", this.todos);
+    },
+    addTodo: function(todo){
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(itemPosition, newValue){
+        this.todos[itemPosition] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(itemPosition){
+        this.todos.splice(itemPosition, 1);
+        this.displayTodos();
     }
 };
 
